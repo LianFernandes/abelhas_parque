17085e44bf9995b51afb2d42330d81cdd3d51a8d
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-03 18:52:25 -0300

    magit commit

--- src/pages/TotemMap.tsx
+++ src/pages/TotemMap.tsx
@@ -1,29 +1,103 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import { FiArrowLeft } from "react-icons/fi";
+import { useHistory } from "react-router-dom";
 
-import logo from '../images/logo.webp';
-import '../styles/pages/totem-map.css';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import logo from "../images/logo.svg";
+import mapMarker from "../images/Map_marker.svg";
+import beeMarker from "../images/honey-bee.svg";
+
+import api from "../services/api";
+
+import "../styles/pages/totem-map.css";
+
+const mapIcon = L.icon({
+  iconUrl: mapMarker,
+  iconSize: [78, 88],
+  iconAnchor: [39, 88],
+  popupAnchor: [170, -6],
+});
+
+const beeIcon = L.icon({
+  iconUrl: beeMarker,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, -6],
+});
+
+interface Totem {
+  id: string;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function TotemMap() {
-    return (
-        <div id="page-map">
-            <nav>
-                <img src={logo} alt="Instituto Abelha Nativa" />
-                <h2>Mapa do Parque</h2>
-            </nav>
+  const [totems, setTotems] = useState<Totem[]>([]);
 
-            <Map
-                center={[-15.8048141, -47.9234958]}
-                zoom={19}
-                style={{ width: '100%', height: '100%' }}
+  useEffect(() => {
+    api.get("totems").then((response) => {
+      setTotems(response.data);
+    });
+  }, []);
+
+  const { goBack } = useHistory();
+
+  return (
+    <div id="page-map">
+      <nav>
+        <img src={logo} alt="Instituto Abelha Nativa" />
+        <h2>Instituto Abelha Nativa</h2>
+        <button type="button" onClick={goBack}>
+          <FiArrowLeft size={24} color="#000" />
+        </button>
+      </nav>
+
+      <Map
+        center={[-15.804664, -47.923214]}
+        zoom={21}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibGlhbmphbiIsImEiOiJja2VhMHRyd2gwMzB6MnRxajB4eHlndTc4In0.3DGlPULlQY-tUXAY2ExRBw`}
+        />
+        {/* url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" */}
+        {/* url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibGlhbmphbiIsImEiOiJja2VhMHRyd2gwMzB6MnRxajB4eHlndTc4In0.3DGlPULlQY-tUXAY2ExRBw`} */}
+
+        <Marker icon={mapIcon} position={[-15.804664, -47.923214]}>
+          <Popup
+            colseButton={false}
+            minWidth={240}
+            masWidth={240}
+            className="map-popup"
+          >
+            Instituto Abelha Nativa
+          </Popup>
+        </Marker>
+
+        {totems.map((totem) => {
+          return (
+            <Marker
+              icon={beeIcon}
+              position={[totem.latitude, totem.longitude]}
+              key={totem.id}
             >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-            </ Map>
-        </div>
-
-    )
+              <Popup
+                colseButton={false}
+                minWidth={240}
+                masWidth={240}
+                className="map-popup"
+              >
+                {totem.name}
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+    </div>
+  );
 }
 
 export default TotemMap;
