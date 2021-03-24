43f692d24fdd5e449a590acfe25f8654610371d9
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 18:08:26 -0300

    tile change

--- src/pages/TotemMap.tsx
+++ src/pages/TotemMap.tsx
@@ -94,10 +94,10 @@ function TotemMap() {
         zoomControl={false}
       >
         <TileLayer
-          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <ZoomControl />
-        {/*url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" */}
+        {/* url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */}
         {/* url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */}
         {/* url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */}
         {/* url={`https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v4/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */}
